a32a605a92591af8f32e764be7d71ad3d77f6d5e
Omar <omar.branez@gmail.com>
2021-08-02 14:00:02 -0400

    commit tree ae172664218bf8588758933da39cf71c247dc854

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    loadDOM();
+});
+
+function loadDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+}
